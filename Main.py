446cdfc129d0ef5af030f5e00f1f45331e7d44c7
Michael_Dremov <michael_dremov@whs1499.local>
2018-11-27 08:01:26 -0500

    Merge branch 'master' of /Users/michael_dremov/PycharmProjects/rubiksCube with conflicts.

--- Main.py
+++ Main.py
@@ -1,2 +1,12 @@
-#test
-print("Hi")
+from tkinter import *
+
+root = Tk()
+
+
+canvas = Canvas(root, width=800, height=500)
+canvas.pack()
+
+square = canvas.create_rectangle(50, 50, 50, 50, fill="red")
+
+
+root.mainloop()
