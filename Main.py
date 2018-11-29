8e8d63f4271025ddbee2bcae66c2caf165677d47
michael_dremov <michael_dremov@student.wayland.k12.ma.us>
2018-11-29 10:13:12 -0500

    dope stuff yeyeye

--- Main.py
+++ Main.py
@@ -2,11 +2,15 @@ from tkinter import *
 
 root = Tk()
 
-
 canvas = Canvas(root, width=800, height=500)
 canvas.pack()
 
-square = canvas.create_rectangle(50, 50, 50, 50, fill="red")
+square = canvas.create_rectangle(50, 50, 100, 100, fill="red")
+
+class Square:
+
 
 
 root.mainloop()
+
+
